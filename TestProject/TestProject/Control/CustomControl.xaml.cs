6e425e6550dc2f5f16b3cc8547cb7ec194877f60
admin <admin@PC5>
2018-05-09 11:07:45 +0530

    Add project files.

---
+++ TestProject/TestProject/Control/CustomControl.xaml.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestProject.Control
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CustomControl : ContentView
+	{
+		public CustomControl ()
+		{
+			InitializeComponent ();
+		    AdjustControlHeight();
+            AdjustControlWidth();
+
+
+		}
+
+        #region Progress Bar Properties
+
+        public static readonly BindableProperty ProgressColorProperty =
+            BindableProperty.Create(
+                nameof(ProgressColor),
+                typeof(Color),
+                typeof(CustomControl),
+                Color.DodgerBlue);
+
+        public Color ProgressColor
+        {
+            get => (Color)GetValue(ProgressColorProperty);
+            set => SetValue(ProgressColorProperty, value);
+        }
+
+
+	    public static readonly BindableProperty BaseColorProperty =
+	        BindableProperty.Create(
+	            nameof(BaseColor),
+	            typeof(Color),
+	            typeof(CustomControl),
+	            Color.Gray);
+	    
+	    public Color BaseColor
+        {
+	        get => (Color)GetValue(BaseColorProperty);
+	        set => SetValue(BaseColorProperty, value);
+	    }
+
+	    public static readonly BindableProperty ProgressProperty =
+	        BindableProperty.Create(
+	            nameof(Progress),
+	            typeof(double),
+	            typeof(CustomControl),
+	            0d);
+
+	    public double Progress
+        {
+	        get => (double)GetValue(ProgressProperty);
+	        set => SetValue(ProgressProperty, value);
+	    }
+
+	    public static readonly BindableProperty ControlHeightProperty =
+	        BindableProperty.Create(
+	            nameof(ControlHeight),
+	            typeof(double),
+	            typeof(CustomControl),
+	            100d);
+
+	    public double ControlHeight
+	    {
+	        get => (double)GetValue(ControlHeightProperty);
+	        set => SetValue(ControlHeightProperty, value);
+	    }
+
+	    public static readonly BindableProperty ControlWidthProperty =
+	        BindableProperty.Create(
+	            nameof(ControlWidth),
+	            typeof(double),
+	            typeof(CustomControl),
+	            100d);
+
+	    public double ControlWidth
+	    {
+	        get => (double)GetValue(ControlWidthProperty);
+	        set => SetValue(ControlWidthProperty, value);
+	    }
+        #endregion
+
+
+        protected override void OnPropertyChanged(string propertyName = null)
+	    {
+	        base.OnPropertyChanged(propertyName);
+
+	        switch (propertyName)
+	        {
+                case "ProgressColor":
+                    ProgressRing.RingProgressColor = ProgressColor;
+                    break;
+	            case "BaseColor":
+	                ProgressRing.RingBaseColor = BaseColor;
+	                break;
+	            case "Progress":
+	                ProgressRing.Progress = Progress;
+	                break;
+	            case "ControlHeight":
+	                ProgressRing.HeightRequest = ControlHeight;
+	                AdjustControlHeight();
+                    break;
+	            case "ControlWidth":
+	                ProgressRing.WidthRequest = ControlWidth;
+	                AdjustControlWidth();
+                    break;
+            }
+	     
+        }
+
+
+        #region Methods
+
+	    private void AdjustControlHeight()
+	    {
+	        BaseImage.HeightRequest = ControlHeight * .8;
+	        ProgressRing.RingThickness = ControlHeight * .08;
+	    }
+	    private void AdjustControlWidth()
+	    {
+	        BaseImage.WidthRequest = ControlWidth * .8;
+	       // ProgressRing.RingThickness = ControlHeight * .08;
+	    }
+        #endregion
+    }
+}
